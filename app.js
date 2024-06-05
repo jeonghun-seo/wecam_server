34d09761594575a799cb4b2344b6f924c7723c7e
seungO <rtd7878@gmail.com>
2024-06-05 07:52:14 +0000

    .

---
+++ app.js
@@ -0,0 +1,12 @@
+requestAnimationFrame("dotenv").config();
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000; 
+//env파일에 포트가 지정되어 있다면 그 포트를 사용하고 지정되어있지 않다면 3000번 포트 사용
+
+app.use("/", require("./routes/main"));
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
